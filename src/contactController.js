e0fad6d04df1eac27df885ef1df4308149f68b01
Álvaro Madrid Lara <alvaro.madrid.lara.contractor@bbva.com>
2019-04-14 17:50:06 +0200

    error controll in contactController

--- src/contactController.js
+++ src/contactController.js
@@ -9,11 +9,13 @@ exports.index = function (req, res) {
                 message: err,
             });
         }
-        res.json({
-            status: "success",
-            message: "Contacts retrieved successfully",
-            data: contacts
-        });
+        else {
+            res.json({
+                status: "success",
+                message: "Contacts retrieved successfully",
+                data: contacts
+            });
+        }
     });
 };
 
@@ -25,51 +27,69 @@ exports.new = function (req, res) {
     contact.email = req.body.email;
     contact.phone = req.body.phone;
 
-// save the contact and check for errors
+    // save the contact and check for errors
     contact.save(function (err) {
-        // if (err)
-        //     res.json(err);
+        if (err) {
+            res.json(err);
+        }
+        else {
 
-res.json({
-            message: 'New contact created!',
-            data: contact
-        });
+            res.json({
+                message: 'New contact created!',
+                data: contact
+            });
+        }
     });
 };
 
 // Handle view contact info
 exports.view = function (req, res) {
     Contact.findById(req.params.contact_id, function (err, contact) {
-        if (err)
+        if (err) {
             res.send(err);
-        res.json({
-            message: 'Contact details loading..',
-            data: contact
-        });
+        }
+        else {
+            res.json({
+                message: 'Contact details loading..',
+                data: contact
+            });
+        }
     });
 };
 
 // Handle update contact info
 exports.update = function (req, res) {
 
-Contact.findById(req.params.contact_id, function (err, contact) {
-        if (err)
+    Contact.findById(req.params.contact_id, function (err, contact) {
+        if (err) {
             res.send(err);
+        }
+        else {
 
-contact.name = req.body.name ? req.body.name : contact.name;
-        contact.gender = req.body.gender;
-        contact.email = req.body.email;
-        contact.phone = req.body.phone;
+            if (contact) {
 
-// save the contact and check for errors
-        contact.save(function (err) {
-            if (err)
-                res.json(err);
-            res.json({
-                message: 'Contact Info updated',
-                data: contact
-            });
-        });
+                contact.name = req.body.name ? req.body.name : contact.name;
+                contact.gender = req.body.gender;
+                contact.email = req.body.email;
+                contact.phone = req.body.phone;
+
+                // save the contact and check for errors
+                contact.save(function (err) {
+                    if (err)
+                        res.json(err);
+                    res.json({
+                        message: 'Contact Info updated',
+                        data: contact
+                    });
+                });
+            }
+            else {
+                res.status(404);
+                res.json({
+                    message: 'request error'
+                });
+            }
+        }
     });
 };
 
@@ -78,12 +98,14 @@ exports.delete = function (req, res) {
     Contact.remove({
         _id: req.params.contact_id
     }, function (err, contact) {
-        if (err)
+        if (err) {
             res.send(err);
-
-res.json({
-            status: "success",
-            message: 'Contact deleted'
-        });
+        }
+        else {
+            res.json({
+                status: "success",
+                message: 'Contact deleted'
+            });
+        }
     });
 };
